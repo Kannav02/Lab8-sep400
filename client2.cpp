c864198eee8f54a505b2dee343a057b936b521f8
Kannav02 <kannavsethi02@gmail.com>
2023-03-30 21:27:51 -0400

    changes made to the file

--- client2.cpp
+++ client2.cpp
@@ -25,87 +25,251 @@ double e;
 double d;
 double phi;
 
-queue<unsigned char*> messageQueue;
-
-pthread_mutex_t lock_x;
-
-void *recv_func(void *arg);
-
-static void shutdownHandler(int sig)
-{
-    switch(sig) {
-        case SIGINT:
-            is_running=false;
-            break;
-    }
+ queue<unsigned char*> messageQueue;
+ 
+ pthread_mutex_t lock_x;
+ pthread_t tid[3];
+ struct arg_struct
+ {
+     int arg1;
+     const unsigned char arg2;
+ 
+ }
+ struct arg_struct args;
+ 
+ void *send_func(void *arg);
+ void *recv_func(void *arg);
+ void *display_func(void *arg);
+ 
+ 
+ static void shutdownHandler(int sig)
+ {
+     switch(sig) {
+         case SIGINT:
+             is_running=false;
+             break;
+     }
+ }
+ 
+ // Returns a^b mod c
+ unsigned char PowerMod(int a, int b, int c)
+ {
+     int res = 1;
+     for(int i=0; i<b; ++i) {
+         res=fmod(res*a, c);
+     }
+     return (unsigned char)res;
+ }
+ 
+ // Returns gcd of a and b
+ int gcd(int a, int h)
+ {
+     int temp;
+     while (1)
+     {
+         temp = a%h;
+         if (temp == 0)
+             return h;
+         a = h;
+         h = temp;
+     }
+ }
+ 
+ // Code to demonstrate RSA algorithm
+ int main()
+ {
+     // Two random prime numbers
+     double p = 11;
+     double q = 23;
+ 
+     // First part of public key:
+     n = p*q;
+ 
+     // Finding other part of public key.
+     // e stands for encrypt
+     e = 2;
+     phi = (p-1)*(q-1);
+     while (e < phi)
+     {
+         // e must be co-prime to phi and
+         // smaller than phi.
+         if (gcd((int)e, (int)phi)==1)
+             break;
+         else
+             e++;
+ 
+     }
+     // Private key (d stands for decrypt)
+     // choosing d such that it satisfies
+     // d*e = 1 + k * totient
+     int k = 2;  // A constant value
+     d = (1 + (k*phi))/e;
+     cout<<"p:"<<p<<" q:"<<q<<" n:"<<n<<" phi:"<<phi<<" e:"<<e<<" d:"<<d<<endl;
+ 
+     const int numMessages=5;
+     const unsigned char messages[numMessages][BUF_LEN]={
+         "House? You were lucky to have a house!",
+         "We used to live in one room, all hundred and twenty-six of us, no furniture.",
+         "Half the floor was missing;",
+         "we were all huddled together in one corner for fear of falling.",
+         "Quit"};
+ 
+     signal(SIGINT, shutdownHandler);
+ 
+     //TODO: Complete the rest
+     int fd,rc;
+     struct sockaddr_in myaddr,clientAddr;
+     if((fd=socket(AF_INET,SOCK_DGRAM,0))==-1)
+     {
+         perror("socket");
+         exit(-1);
+ 
+     }
+     memset(&myaddr,0,sizeof(myaddr));
+     memset(&clientAddr,0,sizeof(clientAddr));
+ 
+     //client 1
+     myaddr.sin_family=AF_INET;
+     rc=inet_pton(AF_INET,IP_ADDR,&myaddr.sin_addr);
+     if(rc==-1)
+     {
+         perror("IP ADDRESS");
+         exit(-1);
+     }
+     myaddr.sin_port=htons(srcPort);
+ 
+     //client2
+     clientAddr.sin_family=AF_INET;
+     rc=inet_pton(AF_INET,IP_ADDR,&clientAddr.sin_addr);
+     if(rc==-1)
+     {
+         perror("client 2 ip");
+ 
+         exit(-1);
+     }
+     clientAddr.sin_port=htons(destPort);
+ 
+     if((bind(fd,(struct sockaddr*)myaddr,sizeof(myaddr)))==-1)
+     {
+         perror("bind");
+         exit(-1);
+ 
+     }
+     args.arg1=fd;
+     args.arg2=messages;
+     is_running=true;
+     pthread_create(&tid[0],NULL,recv_func,&fd);
+     pthread_create(&tid[1],NULL,send_func,&args);
+     pthred_create(&tid[2],NULL,display_func,NULL);
+ 
+ 
+ 
+ 
+ 
+ }
+ void *display_func(void *arg)
+ {
+     unsigned char msg[BUF_LEN];
+     unsigned char quitMessage[4]="Quit";
+     while(is_running)
+     {
+         pthread_mutex_lock(&lock_x);
+         if(messageQueue.isEmpty()==false)
+         {
+             msg=messageQueue.front();
+             messageQueue.pop();
+             if((strcmp(msg,quitMessage))==true)
+             {
+                 is_running=true;
+             }
+ 
+             cout<<msg<<endl;
+         }
+         pthread_mutex_unlock(&lock_x);
+ 
+ 
+     }
+     pthread_exit(0);
+ 
+ 
+ }
+ void *send_func(void *arg)
+ {
+ 
+     int fd=*((int*)arg.arg1);
+     const unsigned char messages=*((const unsigned char*)arg.arg2);
+     sleep(5);
+     double encryptedText[numMessages][BUF_LEN];
+     memset(&encryptedText,0,sizeof(encryptedText));
+ 
+     for(int i =0;i<numMessages;i++)
+     {
+         const unsigned char singleMssg=messages[i];
+ 
+         int j=0;
+         int singleChar=singleMsg[j];
+ 
+ 
+         while(singleChar!=0)
+         {
+             singleChar=singleMsg[j];
+             unsigned char c=PowerMod(singleChar,e,n);
+             encryptedText[i][j]=c;
+             j++;
+ 
+ 
+ 
+         }
+ 
+ 
+     }
+     for(int i=0;i<numMessages;i++)
+     {
+         int numSent=sendto(fd,&encryptedText[i],BUF_LEN,0,(struct sockaddr*)clientAddr,sizeof(clientAddr));
+ #if DEFINED (DEBUG1)
+         cout<<"Num Sent is "<<numSent<<endl;
+ #endif
+         sleep(1);
+     }
+ 
+ pthread_exit(0);
+ 
+ 
+ }
+ 
+ void *recv_func(void *arg)
+ {
+     int fd= *(int*)arg;
+     struct sockaddr_in recvClient;
+     double decryptedText[BUF_LEN];
+     size_t len =sizeof(recvClient);
+     unsigned char msg[BUF_LEN];
+     memset(&decryptedText,0,BUF_LEN);
+ 
+ 
+     while(is_running)
+     {
+         int numRecv=recvfrom(fd,&decryptedText,BUF_LEN,0,(struct sockaddr*)recvClient,len);
+         int i=0;
+         int c=(int)decryptedText[i];
+ 
+         while(c!=0)
+         {
+             msg[i]=(unsigned char)PowerMod(c,d,n);
+             i++;
+ 
+         }
+         pthread_mutex_lock(&lock_x);
+         messageQueue.push(msg);
+         pthread_mutex_unlock(&lock_x);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+     }
 }
-
-// Returns a^b mod c
-unsigned char PowerMod(int a, int b, int c)
-{
-    int res = 1;
-    for(int i=0; i<b; ++i) {
-        res=fmod(res*a, c);
-    }
-    return (unsigned char)res;
-}
-  
-// Returns gcd of a and b
-int gcd(int a, int h)
-{
-    int temp;
-    while (1)
-    {
-        temp = a%h;
-        if (temp == 0)
-          return h;
-        a = h;
-        h = temp;
-    }
-}
-  
-// Code to demonstrate RSA algorithm
-int main()
-{
-    // Two random prime numbers
-    double p = 11;
-    double q = 23;
-  
-    // First part of public key:
-    n = p*q;
-  
-    // Finding other part of public key.
-    // e stands for encrypt
-    e = 2;
-    phi = (p-1)*(q-1);
-    while (e < phi)
-    {
-        // e must be co-prime to phi and
-        // smaller than phi.
-        if (gcd((int)e, (int)phi)==1)
-            break;
-        else
-            e++;
-  
-    }
-    // Private key (d stands for decrypt)
-    // choosing d such that it satisfies
-    // d*e = 1 + k * totient
-    int k = 2;  // A constant value
-    d = (1 + (k*phi))/e;
-    cout<<"p:"<<p<<" q:"<<q<<" n:"<<n<<" phi:"<<phi<<" e:"<<e<<" d:"<<d<<endl;
-
-    const int numMessages=5;
-    const unsigned char messages[numMessages][BUF_LEN]={
-            "You were lucky to have a room. We used to have to live in a corridor.",
-            "Oh we used to dream of livin' in a corridor! Woulda' been a palace to us.",
-            "We used to live in an old water tank on a rubbish tip.",
-            "We got woken up every morning by having a load of rotting fish dumped all over us.",
-            "Quit"};
-
-    signal(SIGINT, shutdownHandler);
-
-    //TODO: Complete the rest
-}
-
-//TODO: Complete the receive thread
