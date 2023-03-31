1616905f039776bc041545cf48eeb52304170409
Kannav Sethi <kannavsethi2@gmail.com>
2023-03-30 23:01:40 -0400

    changes made

--- client1.cpp
+++ client1.cpp
@@ -14,48 +14,42 @@
 
 using namespace std;
 
-const char IP_ADDR[]="127.0.0.1";
-const int BUF_LEN=256;
+const char IP_ADDR[] = "127.0.0.1";
+const int BUF_LEN = 256;
 bool is_running;
-int srcPort=1153;
-int destPort=1155;
+int srcPort = 1153;
+int destPort = 1155;
 // Encrpytion/Decryption variables
 double n;
 double e;
 double d;
 double phi;
 
-queue<unsigned char*> messageQueue;
+queue<unsigned char *> messageQueue;
 
 pthread_mutex_t lock_x;
 pthread_t tid[3];
 
-const int numMessages=5;
-const unsigned char messages[numMessages][BUF_LEN]={
+const int numMessages = 5;
+const unsigned char messageList[numMessages][BUF_LEN] = {
     "House? You were lucky to have a house!",
     "We used to live in one room, all hundred and twenty-six of us, no furniture.",
     "Half the floor was missing;",
     "we were all huddled together in one corner for fear of falling.",
     "Quit"};
-struct threadArgs
-{
-    int arg1;
-    const unsigned char arg2[5][BUF_LEN]=messages;
-
-};
 
 struct sockaddr_in myaddr, clientAddr;
 void *send_func(void *arg);
 void *recv_func(void *arg);
 void *display_func(void *arg);
 
-
 static void shutdownHandler(int sig)
 {
-    switch(sig) {
-        case SIGINT:
-            is_running=false;
-            break;
+    switch (sig)
+    {
+    case SIGINT:
+        is_running = false;
+        break;
     }
 }
 
@@ -63,8 +57,9 @@ static void shutdownHandler(int sig)
 unsigned char PowerMod(int a, int b, int c)
 {
     int res = 1;
-    for(int i=0; i<b; ++i) {
-        res=fmod(res*a, c);
+    for (int i = 0; i < b; ++i)
+    {
+        res = fmod(res * a, c);
     }
     return (unsigned char)res;
 }
@@ -75,7 +70,7 @@ int gcd(int a, int h)
     int temp;
     while (1)
     {
-        temp = a%h;
+        temp = a % h;
         if (temp == 0)
             return h;
         a = h;
@@ -91,215 +86,184 @@ int main()
     double q = 23;
 
     // First part of public key:
-    n = p*q;
+    n = p * q;
 
     // Finding other part of public key.
     // e stands for encrypt
     e = 2;
-    phi = (p-1)*(q-1);
+    phi = (p - 1) * (q - 1);
     while (e < phi)
     {
         // e must be co-prime to phi and
         // smaller than phi.
-        if (gcd((int)e, (int)phi)==1)
+        if (gcd((int)e, (int)phi) == 1)
             break;
         else
             e++;
-
     }
     // Private key (d stands for decrypt)
     // choosing d such that it satisfies
     // d*e = 1 + k * totient
-    int k = 2;  // A constant value
-    d = (1 + (k*phi))/e;
-    cout<<"p:"<<p<<" q:"<<q<<" n:"<<n<<" phi:"<<phi<<" e:"<<e<<" d:"<<d<<endl;
-
+    int k = 2; // A constant value
+    d = (1 + (k * phi)) / e;
+    cout << "p:" << p << " q:" << q << " n:" << n << " phi:" << phi << " e:" << e << " d:" << d << endl;
 
     signal(SIGINT, shutdownHandler);
 
-    //TODO: Complete the rest
-    int fd,rc;
-    if((fd=socket(AF_INET,SOCK_DGRAM,0))==-1)
+    // TODO: Complete the rest
+    int fd, rc;
+    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
     {
         perror("socket");
         exit(-1);
-
     }
-    memset(&myaddr,0,sizeof(myaddr));
-    memset(&clientAddr,0,sizeof(clientAddr));
+    memset(&myaddr, 0, sizeof(myaddr));
+    memset(&clientAddr, 0, sizeof(clientAddr));
 
-    //client 1
-    myaddr.sin_family=AF_INET;
-    rc=inet_pton(AF_INET,IP_ADDR,&myaddr.sin_addr);
-    if(rc==-1)
+    // client 1
+    myaddr.sin_family = AF_INET;
+    rc = inet_pton(AF_INET, IP_ADDR, &myaddr.sin_addr);
+    if (rc == -1)
     {
         perror("IP ADDRESS");
         exit(-1);
     }
-    myaddr.sin_port=htons(srcPort);
+    myaddr.sin_port = htons(srcPort);
 
-    //client2
-    clientAddr.sin_family=AF_INET;
-    rc=inet_pton(AF_INET,IP_ADDR,&clientAddr.sin_addr);
-    if(rc==-1)
+    // client2
+    clientAddr.sin_family = AF_INET;
+    rc = inet_pton(AF_INET, IP_ADDR, &clientAddr.sin_addr);
+    if (rc == -1)
     {
         perror("client 2 ip");
 
         exit(-1);
     }
-    clientAddr.sin_port=htons(destPort);
+    clientAddr.sin_port = htons(destPort);
 
-    if((bind(fd,(struct sockaddr*)&myaddr,sizeof(myaddr)))==-1)
+    if ((bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr))) == -1)
     {
         perror("bind");
         exit(-1);
-
     }
-    struct threadArgs args;
-    args.arg1=fd;
 
-
-    is_running=true;
-    pthread_create(&tid[0],NULL,recv_func,&fd);
-    pthread_create(&tid[1],NULL,send_func,&args);
-    pthread_create(&tid[2],NULL,display_func,NULL);
-
-
-
-
-
+    is_running = true;
+    pthread_create(&tid[0], NULL, recv_func, &fd);
+    pthread_create(&tid[1], NULL, send_func, &fd);
+    pthread_create(&tid[2], NULL, display_func, NULL);
 }
 
-void encrypt(const unsigned char msg[BUF_LEN],double eText[BUF_LEN])
+void encrypt(const unsigned char msg[BUF_LEN], double eText[BUF_LEN])
 {
-    //so the theory here is , we convert each character in the string to its ascii value , then encrypt that char and put it in the double array 
-    //so if our array here of char would be ["abcd\0"], the double array would be [97,98,99,100,0], we will iterate through the char array until we reach 0 ascii value that is , null terminator
+    // so the theory here is , we convert each character in the string to its ascii value , then encrypt that char and put it in the double array
+    // so if our array here of char would be ["abcd\0"], the double array would be [97,98,99,100,0], we will iterate through the char array until we reach 0 ascii value that is , null terminator
 
-    int i=0;
-    int ascii_value=(int)msg[i];
-    while(ascii_value!=0)
+    int i = 0;
+    int ascii_value = (int)msg[i];
+    while (ascii_value != 0)
     {
-        int ascii_encrypted=PowerMod(ascii_value,e,n);
-        eText[i]=double(ascii_encrypted);
+        int ascii_encrypted = PowerMod(ascii_value, e, n);
+        eText[i] = double(ascii_encrypted);
         i++;
-        ascii_value=(int)msg[i];
-
+        ascii_value = (int)msg[i];
     }
-    cout<<msg<<endl;
-    eText[i]=0;
-    cout<<"Encryption is Done"<<endl;
-
-
-
+    cout << msg << endl;
+    eText[i] = 0;
+    cout << "Encryption is Done" << endl;
 }
-void decrypt(double eText[BUF_LEN],unsigned char msg[BUF_LEN])
+void decrypt(double eText[BUF_LEN], unsigned char msg[BUF_LEN])
 {
-    int i=0;
-    int ascii_doubleValue=(int)eText[i];
-    while(ascii_doubleValue!=0)
+    int i = 0;
+    int ascii_doubleValue = (int)eText[i];
+    while (ascii_doubleValue != 0)
     {
-        msg[i]=(unsigned char) PowerMod(ascii_doubleValue,d,n);
+        msg[i] = (unsigned char)PowerMod(ascii_doubleValue, d, n);
         i++;
-        ascii_doubleValue=(int)eText[i];
+        ascii_doubleValue = (int)eText[i];
     }
-    msg[i]='\0';
-    cout<<"Decryption is done"<<endl;
-
-
+    msg[i] = '\0';
+    cout << "Decryption is done" << endl;
 }
 void *display_func(void *arg)
 {
-    unsigned char msg[BUF_LEN];
-    while(is_running)
+    unsigned char *msg;
+    while (is_running)
     {
         pthread_mutex_lock(&lock_x);
-        if(messageQueue.empty()==false)
+        if (messageQueue.empty() == false)
         {
-            msg=*messageQueue.front();
-            delete [] messageQueue.front();
-            messageQueue.front()=nullptr;
+            msg = messageQueue.front();
+            delete[] messageQueue.front();
+            messageQueue.front() = nullptr;
             messageQueue.pop();
-            if((strcmp((char *)msg,"Quit"))==true)
+            if ((strcmp((char *)msg, "Quit")) == true)
             {
-                is_running=false;
+                is_running = false;
             }
 
-            cout<<msg<<endl;
+            cout << msg << endl;
         }
         pthread_mutex_unlock(&lock_x);
-
-
     }
     pthread_exit(0);
-
-
 }
 void *send_func(void *arg)
 {
-    struct threadArgs* local_args=(struct thread_args*)arg;
-    int fd=local_args->arg1;
+    int fd = *(int *)arg;
     // const unsigned char messages[5][256]=local_args->arg2;
 
     sleep(5);
 
     double encryptedText[5][BUF_LEN];
-    memset(&encryptedText,0,sizeof(encryptedText));
+    memset(&encryptedText, 0, sizeof(encryptedText));
 
-    for(int i =0;i<numMessages;i++)
+    for (int i = 0; i < numMessages; i++)
     {
-        encrypt(messages[i],encryptedText[i];
+        encrypt(messageList[i], encryptedText[i]);
+    }
+    for (int i = 0; i < numMessages; i++)
+    {
 
-                } 
-                for(int i=0;i<numMessages;i++)
-                {
-
-                int numSent=sendto(fd,(double *)&encryptedText[i],BUF_LEN,0,(struct sockaddr*)clientAddr,sizeof(clientAddr));
-#if defined (DEBUG1)
-                cout<<"Number of bytes sent are "<<numSent<<endl;
+        int numSent = sendto(fd, (double *)&encryptedText[i], BUF_LEN, 0, (struct sockaddr *)&clientAddr, sizeof(clientAddr));
+#if defined(DEBUG1)
+        cout << "Number of bytes sent are " << numSent << endl;
 #endif
 
-                sleep(1);
+        sleep(1);
+    }
+}
 
-                }
+void *recv_func(void *arg)
+{
+    int fd = *(int *)arg;
+    struct sockaddr_in recvClient;
+    socklen_t len = sizeof(recvClient);
 
+    // we will be convertingt the recieved text(encrypted text) into  decrypted text
+    double recvText[BUF_LEN];
+    unsigned char msg[BUF_LEN];
+    memset(&recvText, 0, BUF_LEN);
+    memset(&msg, 0, BUF_LEN);
 
-                }
+    while (is_running)
+    {
+        int numRecv = recvfrom(fd, &recvText, BUF_LEN, 0, (struct sockaddr *)&recvClient, &len);
 
-                void *recv_func(void *arg)
-                {
-                int fd= *(int*)arg;
-                struct sockaddr_in recvClient;
-                socklen_t len  =sizeof(recvClient);
+        if (numRecv != 0)
+        {
+            decrypt(recvText, msg);
 
+            pthread_mutex_lock(&lock_x);
+            messageQueue.push(msg);
+            pthread_mutex_unlock(&lock_x);
+        }
+        else
+        {
+            sleep(1);
+        }
 
-                //we will be convertingt the recieved text(encrypted text) into  decrypted text
-                double recvText[BUF_LEN];
-                unsigned char msg[BUF_LEN];
-                memset(&recvText,0,BUF_LEN);
-                memset(&msg,0,BUF_LEN);
-
-                while(is_running)
-                {
-                    int numRecv=recvfrom(fd,&recvText,BUF_LEN,0,(struct sockaddr*)&recvClient,len);
-
-                    if(numRecv!=0){
-                        decrypt(recvText,msg);
-
-                        pthread_mutex_lock(&lock_x);
-                        messageQueue.push(msg);
-                        pthread_mutex_unlock(&lock_x);
-                    }
-                    else
-                    {
-                        sleep(1);
-
-                    }
-
-                    //zeroing out the values again
-                    memset(&recvText,0,BUF_LEN);
-                    memset(&msg,0,BUF_LEN);
-
-                }
-
-
-                }
+        // zeroing out the values again
+        memset(&recvText, 0, BUF_LEN);
+        memset(&msg, 0, BUF_LEN);
+    }
+}
